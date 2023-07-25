5d023a67c34748eedbd14cd7dff0e4d2951f651e
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-07-25 19:34:12 -0300

    Added controllers and views for Blogify

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/users', to: 'users#index', as: 'all_users'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get '/posts/:id', to: 'posts#show', as: 'post'
 end
