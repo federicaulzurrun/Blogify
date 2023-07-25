a754319adce658745db40123a8d16dcecf98d38c
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-07-25 20:01:46 -0300

    added optional changes requested by the reviewer

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get '/users', to: 'users#index', as: 'all_users'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/posts/:id', to: 'posts#show', as: 'post'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
