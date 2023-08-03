d33aa9b131e907e298fed356214b9ffdd9c5268a
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-08-03 12:53:37 -0300

    made changes on ability file, solving linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,11 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user.role == "admin"
+    if user.role == 'admin'
       can :manage, :all
     else
-      can :read, Post, Comment 
+      can :read, Post, Comment
       can :create, [Comment, Post, Like]
       can :update, Post, author_id: user.id
       can :destroy, [Comment, Post], author_id: user.id
