a70a7e43bb1681a62e0f676680a3428e49d242f7
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-08-04 14:30:57 -0300

    intalled searialize gem, added post and session controllers, and added post serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -23,5 +21,11 @@ class User < ApplicationRecord
 
   def last_posts
     posts.order(created_at: :desc).limit(5)
+  end
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= 'user'
   end
 end
