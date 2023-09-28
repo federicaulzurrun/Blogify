6fcb04d44191b8dfbddf831e230fa9ca022fb519
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-09-28 16:43:26 -0300

    solutions

--- db/migrate/20230802164935_add_unique_index_to_users_email.rb
+++ db/migrate/20230802164935_add_unique_index_to_users_email.rb
@@ -1,5 +1,7 @@
 class AddUniqueIndexToUsersEmail < ActiveRecord::Migration[7.0]
   def change
-    add_index :users, :email, unique: true
+    unless index_exists?(:users, :email)
+      add_index :users, :email, unique: true
+    end
   end
 end
